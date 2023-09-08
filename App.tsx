50eff6446ef49455fc2acd48d51cb54272e33408
nathslopess <nathalia-silvalopes@outlook.com>
2023-09-08 11:49:08 -0300

    Algumas alterações foram feitas no App.

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/Login';
 import { NativeBaseProvider, StatusBar } from 'native-base'
 import { TEMAS } from './src/styles/temas'
+import Cadastro from './src/Cadastro';
 
 
 export default function App() {
   return (
     <NativeBaseProvider theme={TEMAS}>
       <StatusBar backgroundColor={TEMAS.colors.black}/>
-      <Login/>
+      {/* <Login/> */}
+      <Cadastro/>
     </NativeBaseProvider>
   );
 }
