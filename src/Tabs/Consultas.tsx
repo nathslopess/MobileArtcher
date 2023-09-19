e6dead0a50b298aa225448b503e4acff209566d4
nathslopess <nathalia-silvalopes@outlook.com>
2023-09-19 10:42:22 -0300

    Modelando a tela de consulta dos restaurantes

--- src/Tabs/Consultas.tsx
+++ src/Tabs/Consultas.tsx
@@ -1,11 +1,18 @@
 import { VStack, Text } from "native-base"
+import { Restaurantes } from "../Components/Restaurantes"
+import Consulado from '../assets/consulado2.jpg'
 
 export default function Consultas() {
     return(
-        <VStack>
+        <VStack p={5}>
             <Text>
                 Consulta
             </Text>
+            <Restaurantes
+                nome='Consulado Mineiro'
+                descricao='Especialidade em Comida Mineira'
+                avatar={Consulado}
+            />
         </VStack>
     )
 }
